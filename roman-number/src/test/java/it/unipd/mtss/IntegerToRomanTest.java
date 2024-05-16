189af6294e3c310ef44dcb9b880c18ca7e29b5c6
PiolaAndrea <massiandrea24@gmail.com>
2024-05-16 13:35:38 +0200

    #3 Creazione test per i primi 6 numeri

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -7,17 +7,23 @@ import org.junit.Test;
 public class IntegerToRomanTest {
 
     @Test
-    public void testNumeriFrom1To3() {
+    public void testNumeriFrom1To6() {
         String num1 = IntegerToRoman.convert(1);
         assertEquals("I", num1);
         String num2 = IntegerToRoman.convert(2);
         assertEquals("II", num2);
         String num3 = IntegerToRoman.convert(3);
         assertEquals("III", num3);
+        String num4 = IntegerToRoman.convert(4);
+        assertEquals("IV", num4);
+        String num5 = IntegerToRoman.convert(5);
+        assertEquals("V", num5);
+        String num6 = IntegerToRoman.convert(6);
+        assertEquals("VI", num6);
     }
     @Test
     public void testExceededLimit() {
-        String num = IntegerToRoman.convert(4);
+        String num = IntegerToRoman.convert(7);
         assertNull(num);
     }
 
--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -7,17 +7,23 @@ import org.junit.Test;
 public class RomanPrinterTest {
 
     @Test
-    public void testStampaFrom1To3() {
+    public void testStampaFrom1To6() {
         String printed1 = RomanPrinter.print(1);
         assertEquals("  _____ \n" + " |_   _|\n" + "   | |  \n" + "   | |  \n" + "  _| |_ \n" + " |_____|\n", printed1);
         String printed2 = RomanPrinter.print(2);
         assertEquals("  _____   _____ \n" + " |_   _| |_   _|\n" + "   | |     | |  \n" + "   | |     | |  \n" + "  _| |_   _| |_ \n" + " |_____| |_____|\n", printed2);
         String printed3 = RomanPrinter.print(3);
         assertEquals("  _____   _____   _____ \n" + " |_   _| |_   _| |_   _|\n" + "   | |     | |     | |  \n" + "   | |     | |     | |  \n" + "  _| |_   _| |_   _| |_ \n" + " |_____| |_____| |_____|\n", printed3);
+        String printed4 = RomanPrinter.print(4);
+        assertEquals("  _____  __      __\n" + " |_   _| \\ \\    / /\n" + "   | |    \\ \\  / / \n" + "   | |     \\ \\/ /  \n" + "  _| |_     \\  /   \n" + " |_____|     \\/    \n", printed4);
+        String printed5 = RomanPrinter.print(5);
+        assertEquals(" __      __\n" + " \\ \\    / /\n" + "  \\ \\  / / \n" + "   \\ \\/ /  \n" + "    \\  /   \n" + "     \\/    \n", printed5);
+        String printed6 = RomanPrinter.print(6);
+        assertEquals(" __      __  _____ \n" + " \\ \\    / / |_   _|\n" + "  \\ \\  / /    | |  \n" + "   \\ \\/ /     | |  \n" + "    \\  /     _| |_ \n" + "     \\/     |_____|\n", printed6);
     }
     @Test
     public void testExceededLimit() {
-        String printed = RomanPrinter.print(4);
+        String printed = RomanPrinter.print(7);
         assertEquals(null, printed); 
     }
 
